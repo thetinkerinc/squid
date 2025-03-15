24cc7c13b584a0a4d212f17546b540d858158e40
Shane Brass <shanebrass1618@gmail.com>
2025-03-15 11:14:36 -0600

    Updating gitignore

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C-kyw3Ws.js","_app/immutable/chunks/DbNt_nLg.js","_app/immutable/chunks/HK3yEnHC.js","_app/immutable/chunks/BdkUs9cw.js","_app/immutable/chunks/DSKFzVKS.js"];
-export const stylesheets = [];
-export const fonts = [];
