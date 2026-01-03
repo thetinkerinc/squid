90b5df7ae8a7b2af9a4d388e935e530a48ef9124
Shane Brass <shanebrass1618@gmail.com>
2026-01-03 11:53:41 -0700

    Adding i18n for partner functionality

--- src/routes/dashboard/partners.svelte
+++ src/routes/dashboard/partners.svelte
@@ -4,6 +4,7 @@ let { invitations, partners }: Props = $props();
 import { toast } from 'svelte-sonner';
 import { UserPlus, Check, X } from '@lucide/svelte';
 
+import * as m from '$paraglide/messages';
 import { invite, respond } from '$remote/data.remote';
 
 import formatter from '$utils/formatter';
@@ -26,13 +27,9 @@ async function enhance({ form, submit }: EnhanceParams<typeof invite.enhance>) {
 		await submit();
 		open = false;
 		form.reset();
-		toast.success('Invitation sent!');
+		toast.success(m.partner_add_success());
 	} catch (_err) {
-		const msg =
-			"We couldn't send your invitation. Make " +
-			'sure your partner has an account and that ' +
-			'you spelled their email correctly.';
-		toast.error(msg);
+		toast.error(m.partner_add_error());
 	}
 }
 </script>
@@ -43,25 +40,24 @@ async function enhance({ form, submit }: EnhanceParams<typeof invite.enhance>) {
 			{#snippet child({ props })}
 				<Button {...props}>
 					<UserPlus />
-					Add a partner
+					{m.partner_add_button()}
 				</Button>
 			{/snippet}
 		</AlertDialog.Trigger>
 		<AlertDialog.Content class="border border-white/[0.8] bg-white/[0.3] backdrop-blur">
 			<AlertDialog.Header>
-				<AlertDialog.Title>Add a partner to your account</AlertDialog.Title>
+				<AlertDialog.Title>{m.partner_add_title()}</AlertDialog.Title>
 				<AlertDialog.Description>
-					Partners will be able to see and edit all information you've added up to this point.
-					Likewise, you'll be able to see and edit any information they have added.
+					{m.partner_add_description()}
 				</AlertDialog.Description>
 			</AlertDialog.Header>
 			<form id="invite-partner" {...invite.enhance(enhance)}>
-				<Input placeholder="Email" {...invite.fields.to.as('email')} />
+				<Input placeholder={m.partner_add_email_placeholder()} {...invite.fields.to.as('email')} />
 			</form>
 			<AlertDialog.Footer>
 				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
 				<AlertDialog.Action form="invite-partner" {...invite.buttonProps.enhance(enhance)}>
-					Invite
+					{m.partner_add_invite()}
 				</AlertDialog.Action>
 			</AlertDialog.Footer>
 		</AlertDialog.Content>
@@ -76,10 +72,13 @@ async function enhance({ form, submit }: EnhanceParams<typeof invite.enhance>) {
 	<div class="mt-3 flex flex-wrap items-center gap-3">
 		{#each invitations as invitation (invitation.id)}
 			<div class="inline-block rounded bg-white/[0.7] px-4 py-1 shadow">
-				<div class="underline">Invitation</div>
-				<div>From {invitation.fromEmail}</div>
+				<div class="underline">{m.invitation_title()}</div>
+				<div>{m.invitation_from()} {invitation.fromEmail}</div>
 				<div class="flex items-center gap-2">
-					<div class="mr-4 flex-auto text-gray-500">Sent {formatter.date(invitation.sent)}</div>
+					<div class="mr-4 flex-auto text-gray-500">
+						{m.invitation_sent()}
+						{formatter.date(invitation.sent)}
+					</div>
 					<form {...respond.for('accept')}>
 						<input
 							class="hidden"
